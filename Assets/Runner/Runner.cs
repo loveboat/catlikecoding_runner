6b94559ce019e77a47bf3ddf688e8d1d299c3ca6
David Lusby <lusbyd@gmail.com>
2012-08-24 23:01:29 +0100

    Keep track of the total runner distance travelled
    So we can recycle the skyline accordingly

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class Runner : MonoBehaviour {
 	
+	public static float distanceTraveled;
+	
 	void Update () {
 		transform.Translate(5f * Time.deltaTime, 0f, 0f);
+		
+		distanceTraveled = transform.localPosition.x;
 	}
 }
