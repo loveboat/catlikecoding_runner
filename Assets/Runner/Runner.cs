b92d4094fc290399af79e7356e56db76f4acd395
David Lusby <lusbyd@gmail.com>
2012-08-24 23:40:12 +0100

    Only apply acceleration to the runner when in contact with a platform
    So Runner falls on the flatworm and then moves to the right. It even
    falls again after it moves past the platform. But when it happens to
    collide with the side of the next platform it behaves a bit weird. This
    is because we're still changing its position in an Update method. We
    should leave its movement to the physics engine and instead apply
    forces to it.
    Remove the call to Translate from the Update method of Runner. Instead,
    we'll use two of Unity's collision event methods – OnCollisionEnter and
    OnCollisionExit – to detect when we touch or leave a platform. As long
    as we're touching a platform, we apply an acceleration to make us run
    faster.
    Let's make the acceleration configurable and set it to 5 in the editor.

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -5,9 +5,27 @@ public class Runner : MonoBehaviour {
 	
 	public static float distanceTraveled;
 	
+	public float acceleration;
+
+	private bool touchingPlatform;
+	
 	void Update () {
 		transform.Translate(5f * Time.deltaTime, 0f, 0f);
 		
 		distanceTraveled = transform.localPosition.x;
+	}
+	
+	void FixedUpdate () {
+		if(touchingPlatform){
+			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+		}
+	}
+
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+
+	void OnCollisionExit () {
+		touchingPlatform = false;
 	}
 }
