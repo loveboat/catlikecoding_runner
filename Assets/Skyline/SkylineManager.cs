b99bfdead170d4485ffeb8d138712df926d36445
David Lusby <lusbyd@gmail.com>
2012-08-24 23:09:59 +0100

    Refactored to use a common Recycle method

--- Assets/Skyline/SkylineManager.cs
+++ Assets/Skyline/SkylineManager.cs
@@ -12,23 +12,26 @@ public class SkylineManager : MonoBehaviour {
 
 	void Start () {
 		objectQueue = new Queue<Transform>(numberOfObjects);
+		for(int i = 0; i < numberOfObjects; i++){
+			objectQueue.Enqueue((Transform)Instantiate(prefab));
+		}
 		
 		nextPosition = transform.localPosition;
 		for(int i = 0; i < numberOfObjects; i++){
-			Transform o = (Transform)Instantiate(prefab);
-			o.localPosition = nextPosition;
-			nextPosition.x += o.localScale.x;
-			
-			objectQueue.Enqueue(o);
+			Recycle();
 		}
 	}
 	
 	void Update () {
 		if (objectQueue.Peek().localPosition.x + recycleOffset < Runner.distanceTraveled){
-			Transform o = objectQueue.Dequeue();
-			o.localPosition = nextPosition;
-			nextPosition.x += o.localScale.x;
-			objectQueue.Enqueue(o);
+			Recycle();
 		}
+	}
+	
+	private void Recycle() {
+		Transform o = objectQueue.Dequeue();
+		o.localPosition = nextPosition;
+		nextPosition.x += o.localScale.x;
+		objectQueue.Enqueue(o);
 	}
 }
