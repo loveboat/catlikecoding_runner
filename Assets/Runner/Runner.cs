ac5c5ae2e9ab01fec34333cae1453ac428a9d9bd
David Lusby <lusbyd@gmail.com>
2012-08-24 23:48:32 +0100

    Enabled the runner to jump!
    We'll add a variable to Runner so we can configure its jump velocity.
    We'll use a vector instead of just a float so we can provide both a
    vertical and horizontal component. Set the corresponding field in the
    editor to (1, 7, 0).
    We want Runner to jump only when it's touching a platform while the
    jump button is pressed

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -6,17 +6,22 @@ public class Runner : MonoBehaviour {
 	public static float distanceTraveled;
 	
 	public float acceleration;
+	public Vector3 jumpVelocity;
 
 	private bool touchingPlatform;
 	
 	void Update () {
+		// jump if we're touching the platform
+		if(touchingPlatform && Input.GetButtonDown("Jump")){
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+		}
 		transform.Translate(5f * Time.deltaTime, 0f, 0f);
 		
 		distanceTraveled = transform.localPosition.x;
 	}
 	
 	void FixedUpdate () {
-		if(touchingPlatform){
+		if (touchingPlatform){
 			rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
 		}
 	}
