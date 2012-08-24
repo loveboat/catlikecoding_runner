76c54ddcc8caa7804c17f94fe47e7d27a91a6fc0
David Lusby <lusbyd@gmail.com>
2012-08-24 23:07:21 +0100

    Queue and recycle (reposition) the close-up cubes
    Now we'll store our skyline objects in a queue and keep checking
    whether the first object in it should be recycled. If so, we'll
    reposition it and move it to the back of the queue. Let's use a
    recycleOffset variable to configure how far behind Runner this reuse
    should occur. A value of 60 seems to work well.

--- Assets/Skyline/SkylineManager.cs
+++ Assets/Skyline/SkylineManager.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SkylineManager : MonoBehaviour {
 
 	public Transform prefab;
-	public int numberOfCubes;
+	public int numberOfObjects;
+	public float recycleOffset;
 
 	private Vector3 nextPosition;
+	private Queue<Transform> objectQueue;
 
 	void Start () {
+		objectQueue = new Queue<Transform>(numberOfObjects);
+		
 		nextPosition = transform.localPosition;
-		for(int i = 0; i < numberOfCubes; i++){
+		for(int i = 0; i < numberOfObjects; i++){
 			Transform o = (Transform)Instantiate(prefab);
 			o.localPosition = nextPosition;
 			nextPosition.x += o.localScale.x;
+			
+			objectQueue.Enqueue(o);
+		}
+	}
+	
+	void Update () {
+		if (objectQueue.Peek().localPosition.x + recycleOffset < Runner.distanceTraveled){
+			Transform o = objectQueue.Dequeue();
+			o.localPosition = nextPosition;
+			nextPosition.x += o.localScale.x;
+			objectQueue.Enqueue(o);
 		}
 	}
 }
